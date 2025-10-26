6a345e6e29662f430da13eac1725061dcd0311a0
Qmaymay <13423767696@163.com>
2025-10-26 11:36:50 +0800

    基础C项目-加法乘法

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// 简单的加法函数
+int add(int a, int b) {
+    return a + b;
+}
+
+// 简单的乘法函数
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    printf("Hello, CI/CD World!\n");
+    printf("3 + 4 = %d\n", add(3, 4));
+    printf("3 * 4 = %d\n", multiply(3, 4));
+    return 0;
+}
+
